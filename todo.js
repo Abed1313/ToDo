575adbcf62953adfaeb3c7d13c443d463fa2580c
Abed1313 <abedradwan84.5@gmail.com>
2024-03-20 10:45:14 +0000

    js4

--- todo.js
+++ todo.js
@@ -3,27 +3,33 @@
 
 
 
-var promptResult= prompt("please Enter your name")
-var promptResult2= prompt("please Enter your Gender")
+var name= prompt("please Enter your name")
+var promptResult2= prompt("please Enter your Gender Female or Male")
 var promptResult3= prompt("please Enter your Age")
 
-var confirmResult= confirm("skip the welcoming message")
 
 
 
-if(confirmResult == "Ok"){
-    console.log(alert)
-}else if(confirmResult == "Cancel"){
-   
+if(promptResult3 <=0){
+    alert("age is less than or equal to zero")
 }
+ 
+
+ var confirmResult= confirm("skip the welcoming message")
+console.log(confirmResult)
+
+ if(confirmResult === false){
+
+    if(promptResult2 == "Male"){
+        alert( "Mr." + name)
+    }else if(promptResult2 == "female"){
+        alert( "Ms." + name)
+    }else alert("your Gender mistake")
+ }
 
 
-if(romptResult2 == "Male"){
-    console.log("Mr")
-}else if(romptResult2 == "female"){
-    console.log("Ms")
-}else console.log("your Gender mistake")
 
-alert( promptResult, promptResult2)
+
+
 
 
