1d4b16b22eb773f67454c534fb4d38fbcf39f286
Abed1313 <abedradwan84.5@gmail.com>
2024-03-20 09:45:20 +0000

    js3

---
+++ todo.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+var promptResult= prompt("please Enter your name")
+var promptResult2= prompt("please Enter your Gender")
+var promptResult3= prompt("please Enter your Age")
+
+var confirmResult= confirm("skip the welcoming message")
+
+
+
+if(confirmResult == "Ok"){
+    console.log(alert)
+}else if(confirmResult == "Cancel"){
+   
+}
+
+
+if(romptResult2 == "Male"){
+    console.log("Mr")
+}else if(romptResult2 == "female"){
+    console.log("Ms")
+}else console.log("your Gender mistake")
+
+alert( promptResult, promptResult2)
+
+
